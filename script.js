37d0b94e476e362e9502a99753bb7084c79ae5d9
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-10 19:17:43 +0100

    made corrections to first task

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("registration - form");
+  const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
