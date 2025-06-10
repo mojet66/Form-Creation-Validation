a3c61bef2d5e461302e8959e57581f3f771f775e
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-10 19:19:52 +0100

    made second corrections to first task

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     feedbackDiv.style.display = "block";
     if (isValid) {
-      feedbackDiv.textContent = "Registration Successful";
+      feedbackDiv.textContent = "Registration successful!";
       feedbackDiv.style.color = "#28a745";
     } else {
       feedbackDiv.innerHTML = messages.join("<br>");
